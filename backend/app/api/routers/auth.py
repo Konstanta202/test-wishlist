3d8de076cbd8cf992d59b38af9cd565fa1e9ec7a
Kikos1337 <skalisusov@inbox.ru>
2025-12-06 20:20:30 +0300

    commit

--- backend/app/api/routers/auth.py
+++ backend/app/api/routers/auth.py
@@ -27,7 +27,7 @@ async def auth_telegram(
 ):
     logger.info(f"Received auth_telegram request: {auth_data}")
     init_data = auth_data.get('initData')
-    logger.info(f'THIS IS INIT DATA: {init_data}')
+    print(f'THIS IS INIT DATA: {init_data}')
     if not init_data:
         logger.error("No initData in request")
         raise HTTPException(
