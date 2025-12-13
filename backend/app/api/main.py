4e49366910c00c3f189095d68b75e6254d9a6cf7
Kikos1337 <skalisusov@inbox.ru>
2025-12-13 05:14:44 +0300

    create user routs

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.core.db import init_database, drop_tables
 from app.api.routers.auth import router as auth_routers
+from app.api.routers.user import router as user_routers
 
 
 @asynccontextmanager
@@ -17,7 +18,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_routers, prefix='/api/v1')
-
+app.include_router(user_routers, prefix='/api/v1')
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
